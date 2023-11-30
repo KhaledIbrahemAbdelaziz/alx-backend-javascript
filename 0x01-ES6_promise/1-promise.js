33efca004d2e517191f188e669c7c4377d623e27
KhaledIbrahemAbdelaziz <kk6673326@gmail.com>
2023-11-29 17:01:10 -0800

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,5 @@
 function getFullResponseFromAPI(success) {
-  return new promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'Success' });
     reject(Error('The fake API is not working currently'));
   });
